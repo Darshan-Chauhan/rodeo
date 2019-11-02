6393d712264877fb8e281ee738a2b27ec8bd64cc
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2019-11-02 14:41:59 +0530

    Unused code removed

--- components/UIComponents/Image.js
+++ components/UIComponents/Image.js
@@ -1,10 +1,3 @@
-import styled from "styled-components";
-
-const ImageElement = styled.img`
-  width: ${props => props.height || "300px"};
-  height: ${props => props.height || "450px"};
-`;
-
 const Image = props => (
   <img
     src={props.src}
